4a06c3ec7ee3b196385f8b0caca1970ce140f6ef
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-04-02 11:32:49 -0400

    updating structure to focus on login first

--- server.js
+++ server.js
@@ -1,48 +1,41 @@
-const path = require("path");
-const express = require("express");
-const session = require("express-session");
-const exphbs = require("express-handlebars");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-const {strict} = require("assert");
-const routes = require("./controllers");
-const sequelize = require("./config/connection");
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
 const sess = {
-  secret: "Super duper secret secret",
-  cookie: {
-    maxAge: 360000,
-    httpOnly: true,
-    secure: false,
-    sameSite: "strict",
-  },
+  secret: 'Super secret secret',
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize,
-  }),
+    db: sequelize
+  })
 };
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
-
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({force: false}).then(() => {
-  app.listen(PORT, () =>
-    console.log(
-      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
-    )
-  );
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
--- utils/authenticate.js
+++
@@ -1,9 +0,0 @@
-const ifAuthorized = (req, res, next) => {
-  if (!req.session.loggedIn) {
-    res.redirect("/login");
-  } else {
-    next();
-  }
-};
-
-module.exports = ifAuthorized;
