2739d3161d1ffb5ae21a8e6f6c36a41009481abe
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-04-04 12:26:38 -0400

    deleting a personal blog post is now possible

--- controllers/api/blogpostRoutes.js
+++ controllers/api/blogpostRoutes.js
@@ -17,4 +17,23 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const blogpostData = await Blogpost.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!blogpostData) {
+      res.status(404).json({ message: 'No blogpost found with this id!' });
+      return;
+    }
+
+    res.status(200).json(blogpostData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
--- public/js/blogpersonal.js
+++ public/js/blogpersonal.js
@@ -1,21 +1,37 @@
 const deleteComment = async (event) => {
   event.preventDefault();
   console.log("hey");
-    const cId = event.target.getAttribute("data-cid");
-    const bId = event.target.getAttribute("data-bid");
-    console.log(cId);
-    console.log(bId);
+  const cId = event.target.getAttribute("data-cid");
+  const bId = event.target.getAttribute("data-bid");
+  console.log(cId);
+  console.log(bId);
 
-    const response = await fetch(`/api/comment/${cId}`, {
-      method: "DELETE",
-    });
+  const response = await fetch(`/api/comment/${cId}`, {
+    method: "DELETE",
+  });
 
-    if (response.ok) {
-      document.location.replace(`/blogpost/personal/${bId}`);
-    } else {
-      alert("Failed to delete project");
-    }
-  
+  if (response.ok) {
+    document.location.replace(`/blogpost/personal/${bId}`);
+  } else {
+    alert("Failed to delete comment");
+  }
+};
+
+const deleteBlogpost = async (event) => {
+  event.preventDefault();
+  console.log("hey");
+  const bId = event.target.getAttribute("data-bid");
+  console.log(bId);
+
+  const response = await fetch(`/api/blogpost/${bId}`, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    document.location.replace(`/dashboard`);
+  } else {
+    alert("Failed to delete blogpost");
+  }
 };
 
 document
@@ -27,5 +43,19 @@ document
     }
     if (event.target.getAttribute("data-type") == "comment-delete") {
       deleteComment(event);
+    } else if (event.target.getAttribute("data-type") == "comment-edit") {
+      console.log("still need to add editing feature");
     }
   });
+
+document.querySelector(".blogpost-form").addEventListener("click", (event) => {
+  const isButton = event.target.nodeName === "BUTTON";
+  if (!isButton) {
+    return;
+  }
+  if (event.target.getAttribute("data-type") == "blog-delete") {
+    deleteBlogpost(event);
+  } else if (event.target.getAttribute("data-type") == "blog-edit") {
+    console.log("still need to add editing feature");
+  }
+});
