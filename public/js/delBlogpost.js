96045f9f15ef175893c2bb47413fb747f0d70c8b
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-04-08 22:28:16 -0400

    fixed bug where delete button did not work for user's viewing their blog not in dashboard

--- public/js/delBlogpost.js
+++ public/js/delBlogpost.js
@@ -1,26 +1,25 @@
 const deleteBlogpost = async (event) => {
-    event.preventDefault();
-    console.log("hey");
-    const bId = event.target.getAttribute("data-bid");
-    console.log(bId);
-  
-    const response = await fetch(`/api/blogpost/${bId}`, {
-      method: "DELETE",
-    });
-  
-    if (response.ok) {
-      document.location.replace(`/dashboard`);
-    } else {
-      alert("Failed to delete blogpost");
-    }
-  };
-  
-  document.querySelector(".blogpost-form").addEventListener("click", (event) => {
-    const isButton = event.target.nodeName === "BUTTON";
-    if (!isButton) {
-      return;
-    }
-    if (event.target.getAttribute("data-type") == "blog-delete") {
-      deleteBlogpost(event);
-    } 
+  event.preventDefault();
+
+  const bId = event.target.getAttribute("data-bid");
+
+  const response = await fetch(`/api/blogpost/${bId}`, {
+    method: "DELETE",
   });
+
+  if (response.ok) {
+    document.location.replace(`/dashboard`);
+  } else {
+    alert("Failed to delete blogpost");
+  }
+};
+
+document.querySelector(".blogpost-form").addEventListener("click", (event) => {
+  const isButton = event.target.nodeName === "BUTTON";
+  if (!isButton) {
+    return;
+  }
+  if (event.target.getAttribute("data-type") == "blog-delete") {
+    deleteBlogpost(event);
+  }
+});
